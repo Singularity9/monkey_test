7c329abfb87d6f8ab98b9924dc48697b4bcdd459
Prelang Builder <builder@prelang.com>
2014-07-07 17:59:44 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Film' to allow for create, update

--- app/admin/film.rb
+++ app/admin/film.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Film do
+  permit_params :title, :film_summary, :film_year, :is_released_netflix, :is_released_amazon_stream, :is_released_disc, :netlifx_link, :amazon_stream_link, :disc_link
 
   
   # See permitted parameters documentation:
